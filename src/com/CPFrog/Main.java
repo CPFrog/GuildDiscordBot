9ff1e7fd24d92720009ea80652493c91bafb3571
강개굴 <kokk012@naver.com>
2021-12-24 11:09:33 +0900

    Modularization, Encapsulation

--- src/com/CPFrog/Main.java
+++ src/com/CPFrog/Main.java
@@ -1,40 +1,13 @@
 package com.CPFrog;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.*;
-import org.jsoup.select.Elements;
-
-import java.io.IOException;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        String url="https://lostark.game.onstove.com/Profile/Character/";
+        WebCrawler wc=new WebCrawler();
+        String guild=wc.getGuild();
+        System.out.println(guild);
 
-        Scanner scanner=new Scanner(System.in);
-
-        String name=scanner.next();
-
-        url+=name;
-
-        Document doc= null;
-        try {
-            doc = Jsoup.connect(url).get();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        doc.html();
-
-        Elements elem=doc.select("div[class=\"game-info__guild\"]");
-
-        String guild;
-        int cnt=0;
-        for(Element e:elem.select("span")){
-            if(e.text().equals("길드"))
-                continue;
-            guild=e.text();
-            System.out.println(guild);
-        }
+        WebCrawler wc2=new WebCrawler("바드받으세영");
+        guild=wc2.getGuild();
+        System.out.println(guild);
     }
 }
