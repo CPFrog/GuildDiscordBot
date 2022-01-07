26656a2e17c07ce574dfa71b19e92d897fb034b8
강개굴 <kokk012@naver.com>
2022-01-07 16:09:29 +0900

    initial message added

--- src/main/java/guildServerBot/MessageListener.java
+++ src/main/java/guildServerBot/MessageListener.java
@@ -7,6 +7,7 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.MessageHistory;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.entities.*;
@@ -19,14 +20,27 @@ import java.util.List;
 
 public class MessageListener extends ListenerAdapter {
     memberInfo mi = new memberInfo();
+    String gName = "밤잠";
 
     public static void main(String[] args) throws LoginException {
         DiscordToken dt = new DiscordToken();
         String bot_token = dt.getToken();
         JDA jda = JDABuilder.createDefault(bot_token).build();
         jda.addEventListener(new MessageListener());
+    }
 
-        // TODO: 인증 채널 첫 메시지 띄워주는 함수 구현
+    @Override
+    public void onGuildJoin(GuildJoinEvent event) {
+        Guild guild = event.getGuild();
+        List<TextChannel> tcList = guild.getTextChannelsByName("인증", true);
+        TextChannel tc = tcList.get(0);
+        for (TextChannel tmp : tcList) {
+            if (tmp.getName().equals("인증")) {
+                tc = tmp;
+                break;
+            }
+        }
+        tc.sendMessage("역할을 부여받으려면 '!인증 캐릭터명' 형태로 입력해주세요.").queue();
     }
 
     @Override
@@ -35,35 +49,59 @@ public class MessageListener extends ListenerAdapter {
         TextChannel tc = event.getTextChannel();
         String message = event.getMessage().getContentRaw();
         Guild guild = event.getGuild();
-        if (message.startsWith("!인증") && event.getChannel().getName().equals("인증")) {
+        if (event.getChannel().getName().equals("인증")) {
+            if (message.startsWith("!인증")) {
 //            System.out.println("event :"+event.getMessage().getContentRaw());
-            String gName = "밤잠";
-            Verifier vf = new Verifier(gName);
-            String name = message.split(" ")[1];
-            if (vf.verify(name)) {
-                if (this.mi.signIn(name)) {
-                    Member target = event.getMember();
-                    Role role = guild.getRolesByName("길드원", true).get(0);
-                    guild.modifyNickname(target, name).queue();
-                    guild.addRoleToMember(target.getId(), role).queue(); //ID만 가져오라는데 이거 맞나?
+                Verifier vf = new Verifier(gName);
+                String name = message.split(" ")[1].trim();
+                if (vf.verify(name)) {
+                    if (this.mi.signIn(name)) {
+                        Member target = event.getMember();
+                        Role role = guild.getRolesByName("길드원", true).get(0);
+                        guild.modifyNickname(target, name).queue();
+                        guild.addRoleToMember(target.getId(), role).queue(); //ID만 가져오라는데 이거 맞나?
+                    } else {
+                        tc.sendMessage(name + "님은 이미 인증된 길드원입니다.").queue();
+                        try {
+                            Thread.sleep(1500);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
                 } else {
-                    tc.sendMessage(name + "님은 이미 인증된 길드원입니다.").queue();
+                    tc.sendMessage(name + "님은 " + gName + " 길드에 가입되어있지 않습니다.").queue();
                     try {
                         Thread.sleep(1500);
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
                 }
-            } else {
-                tc.sendMessage(name + "님은 " + gName + " 길드에 가입되어있지 않습니다.").queue();
+                eraser(event, 3);
+            } else if (message.startsWith("!명령어")) {
+                tc.sendMessage("이 봇에서 지원되는 명령어입니다.\n!인증 (캐릭터명) : 길드원 인증 명령어입니다.");
+                try {
+                    Thread.sleep(3000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                eraser(event, 3);
+            }
+            // 임원 이상급 멤버인 경우 검색 길드명 설정 가능하도록 하는 권한 부여. 추후 구현.
+//            else if(message.startsWith("!길드설정")){
+//                List<Member> members=guild.getMembersWithRoles(guild.getRolesByName("길드원",true))
+//                gName = message.split(" ")[1].trim();
+//
+//                tc.sendMessage("길드 이름을 <"+gName+">으로 설정했습니다.");
+//            }
+            else {
+                tc.sendMessage("명령어의 형태가 잘못되었습니다. !명령어 를 사용해서 전체 명령어를 확인하실 수 있습니다.");
                 try {
                     Thread.sleep(1500);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
+                eraser(event, 3);
             }
-            eraser(event, 3);
-            tc.sendMessage("역할을 부여받으려면 '!인증 캐릭터명' 형태로 입력해주세요.").queue();
         } else {
             System.out.printf("[%s] %#s: %s%n", event.getChannel().getName(), event.getAuthor(), event.getMessage().getContentDisplay());
         }
@@ -74,6 +112,7 @@ public class MessageListener extends ListenerAdapter {
         MessageHistory mh = new MessageHistory(tc);
         List<Message> msg = mh.retrievePast(count).complete();
         tc.deleteMessages(msg).complete();
+        tc.sendMessage("역할을 부여받으려면 '!인증 캐릭터명' 형태로 입력해주세요.").queue();
     }
 }
 
