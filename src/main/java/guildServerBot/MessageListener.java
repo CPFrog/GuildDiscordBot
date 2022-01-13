d7175d492854329b1e749285bf44eb0573463824
강개굴 <kokk012@naver.com>
2022-01-13 18:53:02 +0900

    can change criteria guild name, with qualified members

--- src/main/java/guildServerBot/MessageListener.java
+++ src/main/java/guildServerBot/MessageListener.java
@@ -16,6 +16,7 @@ import modules.*;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
 
+import java.util.ArrayList;
 import java.util.List;
 
 //ref: https://blog.naver.com/PostView.naver?blogId=duckhyun4433&logNo=221982334696&parentCategoryNo=&categoryNo=52&viewDate=&isShowPopularPosts=true&from=search
@@ -79,53 +80,51 @@ public class MessageListener extends ListenerAdapter {
                         e.printStackTrace();
                     }
                 }
-                eraser(event, 3);
+                eraser(2, event, 3);
             } else if (message.startsWith("!명령어")) {
-                tc.sendMessage("이 봇에서 지원되는 명령어입니다.\n!인증 (캐릭터명) : 길드원 인증 명령어입니다.");
+                tc.sendMessage("이 봇에서 지원되는 명령어입니다.\n!인증 (캐릭터명) : 길드원 인증 명령어입니다.\n!길드설정 (길드명) : 길드원 여부를 판단하는 길드명을 설정합니다.");
                 try {
                     Thread.sleep(3000);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-                eraser(event, 3);
+                eraser(2, event, 3);
             }
             // 임원 이상급 멤버인 경우 검색 길드명 설정 가능하도록 하는 권한 부여. 추후 구현.
             else if (message.startsWith("!길드설정")) {
-                List<Role> roleList=guild.getRolesByName("길드원",false);
-
-                List<Member> members = guild.getMembersWithRoles(roleList);
+                List<Role> roleList = guild.getRolesByName("길드원", false);
+                roleList.add(guild.getRolesByName("@everyone", false).get(0));
                 String unvarifiedName = message.split(" ")[1].trim();
-                Member sender = event.getMember();
-                System.out.println(guild.getRoles());
-                System.out.println(guild.getMembers());
-                for(Role r:roleList){
-                    System.out.println(r.getName()+" "+r.getId());
+                List<Member> members = new ArrayList<>();
+                for (Role r : roleList) {
+                    List<Member> temp = guild.getMembersWithRoles(r);
+                    members.addAll(temp);
                 }
-                System.out.println(roleList);
-                System.out.println(members);
-                System.out.println(sender);
+                Member sender = event.getMember();
                 if (!members.contains(sender)) {
                     setgName(unvarifiedName);
-                    tc.sendMessage("길드 이름을 <" + gName + ">으로 설정했습니다.");
-                }
-                else{
-                    tc.sendMessage("길드 설정을 변경하려면 임원 이상의 권한이 필요합니다.");
+                    tc.sendMessage("길드 이름을 <" + gName + ">으로 설정했습니다.").queue();
+                    eraser(2, event, 3);
+                } else {
+                    tc.sendMessage("길드 설정을 변경하려면 임원 이상의 권한이 필요합니다.").queue();
+                    eraser(2, event, 3);
                 }
             } else {
-                tc.sendMessage("명령어의 형태가 잘못되었습니다. !명령어 를 사용해서 전체 명령어를 확인하실 수 있습니다.");
-                try {
-                    Thread.sleep(1500);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                eraser(event, 3);
+                tc.sendMessage("명령어의 형태가 잘못되었습니다. !명령어 를 사용해서 전체 명령어를 확인하실 수 있습니다.").queue();
+
+                eraser(2, event, 3);
             }
         } else {
             System.out.printf("[%s] %#s: %s%n", event.getChannel().getName(), event.getAuthor(), event.getMessage().getContentDisplay());
         }
     }
 
-    public void eraser(MessageReceivedEvent event, int count) {
+    public void eraser(float delaySec, MessageReceivedEvent event, int count) {
+        try {
+            Thread.sleep((int) (delaySec * 1000));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         TextChannel tc = event.getTextChannel();
         MessageHistory mh = new MessageHistory(tc);
         List<Message> msg = mh.retrievePast(count).complete();
