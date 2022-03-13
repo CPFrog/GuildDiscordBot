87461cbbb5345401925d742f74d53a9188acd864
강개굴 <kokk012@naver.com>
2022-03-13 23:23:09 +0900

    [220313] 모듈 추가 - 동일한 이름을 가지면서 유사한 역할을 수행하는 메소드/모듈을 하나의 공통모듈로 분리하기 위한 추가적인 모듈 class 추가.

---
+++ src/main/java/modules/Common.java
@@ -0,0 +1,7 @@
+/* Common : 일반 길드원용, 길드 관리자용 모듈에서 공통적으로 존재하는 기능을 모은 모듈
+   TODO: 1. eraser, 2. 사사게 검색, 3. 명령어 오류 안내, 4. 공통변수(a.k.a. 길드 이름) 추후 여기로 옮겨오기.
+ */
+package modules;
+
+public class Common {
+}
